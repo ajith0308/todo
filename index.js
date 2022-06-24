03841522faf278e901d6702ce67712a39ec1258b
ajith s <62297509+ajith0308@users.noreply.github.com>
2022-06-24 16:48:15 +0530

    firebaseworkcompleted

--- index.js
+++ index.js
@@ -1,51 +1,108 @@
-var task = [];
-var completedtask = [];
+//insert into firebase
+async function inserttofirebase(tasklist) {
+  var raw = JSON.stringify(tasklist);
+  var requestOptions = {
+    method: "POST",
+    body: raw,
+  };
 
-function inserttofirebase() {}
-function deletelist(e) {
+  let response = await fetch(
+    "https://todoapp-730b4-default-rtdb.firebaseio.com/main.json",
+    requestOptions
+  );
+  let data = await response.json();
+  //console.log(data);
+  return data;
+}
+//insert into completed list
+function completedlist(e) {
   let parentElement = e.parentElement; ///.  get attributes
   let parentElementID = e.parentElement.id;
   let time = parentElement.querySelector(".time");
   let title = parentElement.querySelector(".task_name");
+  firebaseupdate(parentElementID);
   document.querySelector(
     "#v-pills-profile"
-  ).innerHTML += `<s><div id='${parentElementID}'>
+  ).innerHTML += `<div id='${parentElementID}' class="completed">
   <div class="task_completed" >${title.textContent}</div>
   <div class="time">${time.textContent}</div>
 </div>`;
 
-  console.log("this" + time.textContent);
-  console.log("this" + title.textContent);
-  console.log("this" + parentElementID);
-  completedtask.push({
-    id: parentElementID,
-    title: title,
-    time: time,
-    status: "completed",
-  });
-  console.log(completedtask);
-
   e.parentElement.remove();
 }
+//insert into Tasklist
 function addTask() {
-  title = document.querySelector("#taskname").value;
-  time = document.querySelector("#time").value;
-  ids = Date.now();
-  let x = {
-    id: ids,
+  let responsevalue;
+  let title = document.querySelector("#taskname").value;
+  let time = document.querySelector("#time").value;
+  let response = inserttofirebase({
     title: title,
     datetime: time,
     status: "Not Completed",
-  };
-  task.push(x);
-  console.log(title);
-  console.log(window.title);
-  console.log(task);
+  });
+
+  response.then((responseResult) => {
+    responsevalue = responseResult.name;
+  });
   //   console.log("this is" + title);
-  document.querySelector("#v-pills-home").innerHTML += `<div id='${ids}'>
+  document.querySelector(
+    "#v-pills-home"
+  ).innerHTML += `<div id='${responsevalue}'>
   <div class="task_name" >${title}</div>
   <div class="time">${time}</div>
-  <input type="checkbox" onclick="deletelist(this)">
+  <input type="checkbox" onclick="completedlist(this)">
 </div>`;
   document.querySelector("#addtask").setAttribute("data-bs-dismiss", "modal");
+}
+
+//get data form firebase
+function firebasefetch() {
+  var requestOptions = {
+    method: "GET",
+  };
+
+  fetch(
+    "https://todoapp-730b4-default-rtdb.firebaseio.com/main.json",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      for (let key in result) {
+        if (result[key].status === "Not Completed") {
+          document.querySelector(
+            "#v-pills-home"
+          ).innerHTML += `<div id='${key}'>
+  <div class="task_name" >${result[key].title}</div>
+  <div class="time">${result[key].datetime}</div>
+  <input type="checkbox" onclick="completedlist(this)">
+</div>`;
+        } else {
+          document.querySelector(
+            "#v-pills-profile"
+          ).innerHTML += `<div id='${key}'>
+  <div class="task_completed">${result[key].title}</div>
+  <div class="time">${result[key].datetime}</div>
+</div>`;
+        }
+      }
+    });
+}
+
+function firebaseupdate(parentElementID) {
+  var raw = JSON.stringify({
+    status: "Completed",
+  });
+
+  var requestOptions = {
+    method: "PATCH",
+    body: raw,
+  };
+
+  fetch(
+    `https://todoapp-730b4-default-rtdb.firebaseio.com/main/${parentElementID}.json`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 }
